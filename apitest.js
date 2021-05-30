73930dffa5d5ba30ec94c9d78206725d34bef86c
Aaron Curtis <aaron.curtis@jpl.nasa.gov>
2021-05-29 18:35:32 -0700

    Say yes or no

--- apitest.js
+++ apitest.js
@@ -1,4 +1,4 @@
-const apikey = '5114ed37436b479bb6e0b58a920b9111'
+const apikey = 'da5fa962f528d64731c3ad0c3c90ca4e'
 
 const hoursAgo = (hours)=>{
   const now = new Date()
@@ -12,9 +12,8 @@ const rainylon=-75
 const rainytime = 1622249755
 
 const getWeather = async (timestamp, lat, lon)=>{
-  await fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${timestamp.getMilliseconds()/1000}&appid=5114ed37436b479bb6e0b58a920b9111`)
+  return await fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${Math.round(timestamp.getTime() / 1000)}&appid=${apikey}`)
     .then(res=>res.json())
-    .then(res=>{console.log(res)})
 }
 
 const checkForRain = async (timestamp, lat, lon)=>{
@@ -30,7 +29,13 @@ const checkForRain = async (timestamp, lat, lon)=>{
   return it_rained
 }
 
-// document.body.innerText = `It is now ${new Date()} but 1 hour ago it was ${hoursAgo(1)}`
-document.body.innerText += `It is now ${new Date()} but 1 hour ago it was ${hoursAgo(1)}`
-document.body.innerText += `The weather was ${getWeather(hoursAgo(1))}`
-if (checkForRain){document.body.innerText += `IT RAINED`}
+const checkRainAndDisplay = async (geolocation)=>{
+  if(await checkForRain(hoursAgo(12), geolocation.coords.latitude, geolocation.coords.longitude)){
+    document.body.innerText = 'YES'
+  } else {
+    document.body.innerText = 'NO'
+  }
+}
+
+window.navigator.geolocation
+  .getCurrentPosition(checkRainAndDisplay, console.log);
